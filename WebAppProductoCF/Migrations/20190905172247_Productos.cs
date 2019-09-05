2c5f348eb314b862b4a8d62725191345e9e2b943
Diego-Tobar <54144333+Diego-Tobar@users.noreply.github.com>
2019-09-05 13:17:51 -0500

    Initial commit

---
+++ WebAppProductoCF/Migrations/20190905172247_Productos.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppProductoCF.Migrations
+{
+    public partial class Productos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebAppProductoCF/Models/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppProductoCF.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required(ErrorMessage = "Ingrese un nombre.")]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "El nombtre no debe tener mas de 50 caracteres, ni menos de 3 caracteres")]
+
+        public string Nombre { get; set; }
+        [StringLength(255,
+            ErrorMessage = "Ladescrpcion no debe tener mas 26556 caracteres")]
+        [Display(Name = "Descripcion")]
+
+        public string Descripcion { get; set; }
+        public bool? Estado { get; set; }
+        public virtual ICollection<Producto> Producto { get; set; }
+
+    }
+}
